55f859a9d6c15224a56e5eb67175d126e212d8f7
Rohan Batra <rohanbatra.in@gmail.com>
2025-11-09 14:34:53 +0530

    fixed

--- src/new_main.cpp
+++ src/new_main.cpp
@@ -9,47 +9,45 @@
 #include <algorithm>
 #include <U8g2lib.h>
 #include "pins.h"
-#include<ACS712.h>
+#include <ACS712.h>
+
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 ACS712 cur(curSensorPin, 3.3, 4095, 66);
 float front_distance, left_distance, right_distance;
 float battery = 0.0;
-void showStatus(const String &mode, int speed) {
+void showStatus(const String &mode, int speed)
+{
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_6x12_tf);
 
-    
     String batteryStr = String(battery, 1) + "V";
     int bw = u8g2.getStrWidth(batteryStr.c_str());
     u8g2.drawStr(128 - bw - 2, 10, batteryStr.c_str());
 
-    
     u8g2.drawStr(2, 14, ("MODE: " + mode).c_str());
 
-    
     u8g2.drawStr(2, 30, ("SPD: " + String(speed) + "%").c_str());
 
     u8g2.sendBuffer();
 }
 
-void showAlert(const String &mainMessage, const String &subHeading = "") {
+void showAlert(const String &mainMessage, const String &subHeading = "")
+{
     u8g2.clearBuffer();
 
-    
     String batteryStr = String(battery, 1) + "V";
     u8g2.setFont(u8g2_font_6x12_tf);
     int bw = u8g2.getStrWidth(batteryStr.c_str());
     u8g2.drawStr(128 - bw - 2, 10, batteryStr.c_str());
 
-    
-    u8g2.setFont(u8g2_font_logisoso16_tf);  
+    u8g2.setFont(u8g2_font_logisoso16_tf);
     int w = u8g2.getStrWidth(mainMessage.c_str());
     int x = (128 - w) / 2;
-    int y = (subHeading.isEmpty()) ? 38 : 30;  
+    int y = (subHeading.isEmpty()) ? 38 : 30;
     u8g2.drawStr(x, y, mainMessage.c_str());
 
-    
-    if (subHeading.length() > 0) {
+    if (subHeading.length() > 0)
+    {
         u8g2.setFont(u8g2_font_6x12_tf);
         int w2 = u8g2.getStrWidth(subHeading.c_str());
         int x2 = (128 - w2) / 2;
@@ -59,13 +57,105 @@ void showAlert(const String &mainMessage, const String &subHeading = "") {
     u8g2.sendBuffer();
 }
 
+// class MotorDriver
+// {
+// private:
+//     int ena, in1, in2, enb, in3, in4, chA, chB, speed;
+
+// public:
+//     MotorDriver(int ena, int in1, int in2, int enb, int in3, int in4, int speed, int chA = 3, int chB = 4)
+//     {
+//         this->ena = ena;
+//         this->in1 = in1;
+//         this->in2 = in2;
+//         this->enb = enb;
+//         this->in3 = in3;
+//         this->in4 = in4;
+//         this->chA = chA;
+//         this->chB = chB;
+//         this->speed = speed;
+//     }
+
+//     void begin()
+//     {
+//         pinMode(in1, OUTPUT);
+//         pinMode(in2, OUTPUT);
+//         pinMode(in3, OUTPUT);
+//         pinMode(in4, OUTPUT);
+
+//         ledcAttachPin(ena, chA);
+//         ledcAttachPin(enb, chB);
+//         ledcSetup(chA, 300, 8);
+//         ledcSetup(chB, 300, 8);
+//         stop();
+//     }
+
+//     void setSpeed(int s)
+//     {
+//         speed = constrain(s, 0, 255);
+//         ledcWrite(chA, speed);
+//         ledcWrite(chB, speed);
+//     }
+
+//     void forward()
+//     {
+//         digitalWrite(in1, HIGH);
+//         digitalWrite(in2, LOW);
+//         digitalWrite(in3, HIGH);
+//         digitalWrite(in4, LOW);
+//         ledcWrite(chA, speed);
+//         ledcWrite(chB, speed);
+//     }
+
+//     void backward()
+//     {
+//         digitalWrite(in1, LOW);
+//         digitalWrite(in2, HIGH);
+//         digitalWrite(in3, LOW);
+//         digitalWrite(in4, HIGH);
+//         ledcWrite(chA, speed);
+//         ledcWrite(chB, speed);
+//     }
+
+//     void turnLeft()
+//     {
+//         digitalWrite(in1, HIGH);
+//         digitalWrite(in2, LOW);
+//         digitalWrite(in3, LOW);
+//         digitalWrite(in4, HIGH);
+//         ledcWrite(chA, speed / 2);
+//         ledcWrite(chB, speed);
+//     }
+
+//     void turnRight()
+//     {
+//         digitalWrite(in1, LOW);
+//         digitalWrite(in2, HIGH);
+//         digitalWrite(in3, HIGH);
+//         digitalWrite(in4, LOW);
+//         ledcWrite(chA, speed);
+//         ledcWrite(chB, speed / 2);
+//     }
+
+//     void stop()
+//     {
+//         digitalWrite(in1, LOW);
+//         digitalWrite(in2, LOW);
+//         digitalWrite(in3, LOW);
+//         digitalWrite(in4, LOW);
+//         ledcWrite(chA, 0);
+//         ledcWrite(chB, 0);
+//     }
+// };
 class MotorDriver
 {
 private:
-    int ena, in1, in2, enb, in3, in4, chA, chB, speed;
+    int ena, in1, in2; // Right side
+    int enb, in3, in4; // Left side
+    int chA, chB, speed;
 
 public:
-    MotorDriver(int ena, int in1, int in2, int enb, int in3, int in4, int speed,int chA = 3, int chB = 4)
+    MotorDriver(int ena, int in1, int in2, int enb, int in3, int in4, int speed, int chA = 3, int chB = 4)
     {
         this->ena = ena;
         this->in1 = in1;
@@ -85,58 +175,61 @@ public:
         pinMode(in3, OUTPUT);
         pinMode(in4, OUTPUT);
 
+        ledcSetup(chA, 300, 8);
+        ledcSetup(chB, 300, 8);
         ledcAttachPin(ena, chA);
         ledcAttachPin(enb, chB);
-        ledcSetup(chA, 1000, 8);
-        ledcSetup(chB, 1000, 8);
+
         stop();
     }
 
     void setSpeed(int s)
     {
         speed = constrain(s, 0, 255);
-        ledcWrite(chA, speed);
-        ledcWrite(chB, speed);
     }
 
     void forward()
     {
+        // Both sides forward
         digitalWrite(in1, HIGH);
         digitalWrite(in2, LOW);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, HIGH);
         ledcWrite(chA, speed);
         ledcWrite(chB, speed);
     }
 
     void backward()
     {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-        ledcWrite(chA, speed);
-        ledcWrite(chB, speed);
-    }
-
-    void turnLeft()
-    {
+        // Both sides backward
         digitalWrite(in1, LOW);
         digitalWrite(in2, HIGH);
         digitalWrite(in3, HIGH);
         digitalWrite(in4, LOW);
-        ledcWrite(chA, speed / 2);
+        ledcWrite(chA, speed);
         ledcWrite(chB, speed);
     }
 
-    void turnRight()
+    void left()
     {
-        digitalWrite(in1, HIGH);
+        // Right side forward, left side backward
+        digitalWrite(in1, HIGH); // Right forward
         digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
+        digitalWrite(in3, HIGH); // Left backward
+        digitalWrite(in4, LOW);
+        ledcWrite(chA, speed);
+        ledcWrite(chB, speed);
+    }
+
+    void right()
+    {
+        // Right side backward, left side forward
+        digitalWrite(in1, LOW); // Right backward
+        digitalWrite(in2, HIGH);
+        digitalWrite(in3, LOW); // Left forward
         digitalWrite(in4, HIGH);
         ledcWrite(chA, speed);
-        ledcWrite(chB, speed / 2);
+        ledcWrite(chB, speed);
     }
 
     void stop()
@@ -150,53 +243,59 @@ public:
     }
 };
 
+void showDistance(float distance)
+{
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_logisoso16_tf);
+    u8g2.drawStr(10, 30, "DISTANCE:");
+
+    char buf[16];
+    sprintf(buf, "%.1f cm", distance);
+    u8g2.setFont(u8g2_font_logisoso22_tf);
+    u8g2.drawStr((128 - u8g2.getStrWidth(buf)) / 2, 60, buf);
+
+    u8g2.sendBuffer();
+}
 class HCSR04
 {
 private:
-    int trigPin, echoPin;
-    static constexpr float SOUND_SPEED = 0.0343;
-    static constexpr float MAX_DISTANCE = 200.0;
+    int trigPin;
+    int echoPin;
+    int calibration = 20;
 
 public:
-    HCSR04(int trig, int echo) : trigPin(trig), echoPin(echo) {}
+    HCSR04(int trig, int echo)
+    {
+        trigPin = trig;
+        echoPin = echo;
+    }
 
     void begin()
     {
         pinMode(trigPin, OUTPUT);
         pinMode(echoPin, INPUT);
-    }
-
-    float raw_getDistance()
-    {
         digitalWrite(trigPin, LOW);
-        delayMicroseconds(5);
-        digitalWrite(trigPin, HIGH);
-        delayMicroseconds(15);
-        digitalWrite(trigPin, LOW);
-
-        long duration = pulseIn(echoPin, HIGH, 30000);
-        if (duration == 0)
-            return 999.0;
-
-        float dis = duration * SOUND_SPEED / 2.0;
-        return (dis <= MAX_DISTANCE ? dis : MAX_DISTANCE);
-    }
-
-    float getFilteredDistance()
-    {
-        float readings[3];
-        for (int i = 0; i < 3; i++)
-        {
-            readings[i] = raw_getDistance();
-            delay(10);
-        }
-        std::sort(readings, readings + 3);
-        return readings[1];
+        delay(50);
     }
 
     float getDistance()
     {
-        return getFilteredDistance();
+        // Send trigger pulse (10µs)
+        digitalWrite(trigPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin, LOW);
+
+        // Measure echo pulse
+        long duration = pulseIn(echoPin, HIGH, 30000); // 30ms timeout
+        if (duration == 0)
+            return -1; // No object detected
+
+        // Calculate distance (cm)
+        float distance = (duration * 0.0343) / 2.0;
+        float corrected = distance - calibration;
+        return corrected <= 0 ? 0 : corrected;
     }
 };
 
@@ -206,13 +305,18 @@ private:
     MotorDriver motor;
     HCSR04 scanner;
     Servo servo;
-    const int threshold = 30;
+    const int threshold = 40;
     int front, back;
-    int motorspeed = 180;
+    int motorspeed = 200;
     int irsensor;
 
 public:
-    int getmotorspeed(){
+    HCSR04 getscanner()
+    {
+        return scanner;
+    }
+    int getmotorspeed()
+    {
         return this->motorspeed;
     }
     float getdis(int angle)
@@ -229,7 +333,7 @@ public:
     }
     CAR(int ena, int in1, int in2, int enb, int in3, int in4,
         int trig, int echo, int servoPin, int front_led, int back_led, int irsensor1)
-        : motor(ena, in1, in2, enb, in3, in4,motorspeed), scanner(trig, echo)
+        : motor(ena, in1, in2, enb, in3, in4, motorspeed), scanner(trig, echo)
     {
         front = front_led;
         back = back_led;
@@ -241,7 +345,7 @@ public:
         pinMode(front, OUTPUT);
         pinMode(back, OUTPUT);
         pinMode(irsensor, INPUT);
-        
+        scanner.begin();
         motor.begin();
         motor.setSpeed(motorspeed);
         servo.write(90);
@@ -276,7 +380,7 @@ public:
         digitalWrite(front, HIGH);
         digitalWrite(back, LOW);
         motor.setSpeed(speed);
-        motor.turnLeft();
+        motor.left();
     }
 
     void turnRight(int speed)
@@ -284,7 +388,7 @@ public:
         digitalWrite(front, HIGH);
         digitalWrite(back, LOW);
         motor.setSpeed(speed);
-        motor.turnRight();
+        motor.right();
     }
 
     void spinAround(int speed)
@@ -292,13 +396,12 @@ public:
         digitalWrite(front, HIGH);
         digitalWrite(back, HIGH);
         motor.setSpeed(speed);
-        motor.turnLeft();
+        motor.left();
         delay(1500);
         stop();
     }
     void adasDrive()
     {
-
         if (digitalRead(irsensor) == LOW)
         {
             stop();
@@ -316,7 +419,7 @@ public:
             int turnDir = random(0, 2);
             Serial.printf("IR escape: Turning %s\n", turnDir == 0 ? "LEFT" : "RIGHT");
 
-            int turnSpeed = 220;
+            int turnSpeed = 180;
             int turnTime = 800;
 
             if (turnDir == 0)
@@ -342,11 +445,38 @@ public:
             return;
         }
 
+        // front_distance = scanner.getDistance();
+        // int temp = constrain(int(front_distance), 10, 200);
+        // motorspeed = map(temp, 10, 200, 100, 200);
         front_distance = scanner.getDistance();
-        int temp = constrain(int(front_distance), 10, 200);
-        motorspeed = map(temp, 10, 200, 100, 200);
 
-        Serial.printf("Front Distance: %.2f cm | Speed: %d\n", front_distance, motorspeed);
+        int safeDistance = threshold + 20; // start accelerating after this
+
+        if (front_distance <= threshold)
+        {
+            // Too close — stop
+            motorspeed = 0;
+        }
+        else if (front_distance <= safeDistance)
+        {
+            // Between threshold and safe distance → slow zone
+            motorspeed = map(front_distance, threshold, safeDistance, 100, 150);
+        }
+        else if (front_distance <= 200)
+        {
+            // Beyond safe zone → cruise zone
+            motorspeed = map(front_distance, safeDistance, 200, 150, 200);
+        }
+        else
+        {
+            // Cap at max speed
+            motorspeed = 200;
+        }
+
+        motorspeed = constrain(motorspeed, 0, 200);
+        Serial.printf("Front Distance: %.2f cm | Adaptive Speed: %d\n", front_distance, motorspeed);
+
+        // Serial.printf("Front Distance: %.2f cm | Speed: %d\n", front_distance, motorspeed);
 
         if (front_distance > threshold)
         {
@@ -370,30 +500,36 @@ public:
             delay(200);
 
             Serial.printf("Left: %.2f cm | Right: %.2f cm\n", left_distance, right_distance);
-
+            // if (left_distance == -1 || front_distance == -1 || right_distance == -1)
+            // {
+            //     while (true)
+            //     {
+            //         showAlert("Error Hcsr04");
+            //     }
+            // }
             if (left_distance > threshold && left_distance > right_distance)
             {
-                int turnDur = map(left_distance, threshold, 200, 500, 1000);
+                int turnDur = map(left_distance, threshold, 200, 300, 700);
                 Serial.printf("Turning LEFT for %d ms\n", turnDur);
                 backward(175);
                 delay(150);
-                turnLeft(255);
+                turnLeft(180);
                 delay(turnDur);
             }
             else if (right_distance > threshold)
             {
-                int turnDur = map(right_distance, threshold, 200, 500, 1000);
+                int turnDur = map(right_distance, threshold, 200, 300, 700);
                 Serial.printf("Turning RIGHT for %d ms\n", turnDur);
                 backward(175);
                 delay(150);
-                turnRight(255);
+                turnRight(180);
                 delay(turnDur);
             }
             else
             {
                 Serial.println("No clear side. Reversing and random escape...");
 
-                int reverseTime = 1000;
+                int reverseTime = 700;
                 int reverseSpeed = 200;
                 backward(reverseSpeed);
                 delay(reverseTime);
@@ -401,20 +537,20 @@ public:
                 delay(100);
 
                 int turnDir = random(0, 2);
-                int turnTime = 1000;
+                int turnTime = 500;
 
                 Serial.printf("Random escape: Turning %s for %d ms\n", turnDir == 0 ? "LEFT" : "RIGHT", turnTime);
                 if (turnDir == 0)
                 {
                     backward(175);
                     delay(150);
-                    turnLeft(255);
+                    turnLeft(180);
                 }
                 else
                 {
                     backward(175);
                     delay(150);
-                    turnRight(255);
+                    turnRight(180);
                 }
                 delay(turnTime);
                 stop();
@@ -468,7 +604,7 @@ public:
         }
         else if (command == "Spin")
         {
-            spinAround(speed);
+            spinAround(255);
         }
         if (command != "S")
         {
@@ -495,6 +631,7 @@ public:
             }
             else if (cmd.equalsIgnoreCase("servo"))
             {
+                Serial.println("testing servo");
                 servotest();
             }
             else if (cmd.equalsIgnoreCase("stop"))
@@ -515,6 +652,11 @@ public:
             Serial.println("========================");
             delay(500);
         }
+    }
+    void hcsr04test()
+    {
+        float dis = scanner.getDistance();
+        showDistance(dis);
     }
 };
 
@@ -646,11 +788,11 @@ float getBatteryVoltage()
 void showBootScreen()
 {
     u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_logisoso16_tr); 
+    u8g2.setFont(u8g2_font_logisoso16_tr);
     const char *title = "ADAS CAR";
     u8g2.drawStr((128 - u8g2.getStrWidth(title)) / 2, 28, title);
 
-    u8g2.setFont(u8g2_font_6x12_tf); 
+    u8g2.setFont(u8g2_font_6x12_tf);
     const char *subtitle = "By Rohan Batra";
     u8g2.drawStr((128 - u8g2.getStrWidth(subtitle)) / 2, 50, subtitle);
 
@@ -680,9 +822,41 @@ void setup()
     Serial.println("All Handlers Initialized");
     server.begin();
     Serial.println("Server started");
+    Serial.println("testing servo");
+    mycar.servotest();
     delay(2000);
 }
 int count = 0;
+void showDistances(float left, float front, float right)
+{
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_6x12_tf); // small clean font
+
+    // --- Title ---
+    const char *title = "DISTANCE SCAN";
+    int titleW = u8g2.getStrWidth(title);
+    u8g2.drawStr((128 - titleW) / 2, 10, title);
+
+    // --- Front ---
+    String fStr = "FRONT: " + String(front, 1) + " cm";
+    u8g2.drawStr(10, 28, fStr.c_str());
+
+    // --- Left ---
+    String lStr = "LEFT:  " + String(left, 1) + " cm";
+    u8g2.drawStr(10, 44, lStr.c_str());
+
+    // --- Right ---
+    String rStr = "RIGHT: " + String(right, 1) + " cm";
+    u8g2.drawStr(10, 60, rStr.c_str());
+
+    // --- Optional battery indicator (top-right) ---
+    String batteryStr = String(battery, 1) + "V";
+    int bw = u8g2.getStrWidth(batteryStr.c_str());
+    u8g2.drawStr(128 - bw - 2, 10, batteryStr.c_str());
+
+    u8g2.sendBuffer();
+}
+
 void loop()
 {
 
@@ -694,15 +868,17 @@ void loop()
         {
             // if (count == 0)
             // {
-                
+
             //     // showStatus("ADAS", mycar.getmotorspeed());
             //     count++;
             // }
-            showAlert("ADAS");
+            // showAlert("ADAS");
             mycar.adasDrive();
+            showDistances(left_distance, front_distance, right_distance);
             battery = getBatteryVoltage();
         }
-        else{
+        else
+        {
             showAlert("MANUAL");
         }
     }
@@ -712,6 +888,8 @@ void loop()
         mycar.stop();
         showAlert("IGN OFF");
     }
+    // mycar.hcsr04test();
     mycar.handleSerialCommands();
     battery = getBatteryVoltage();
+    front_distance = mycar.getdis(90);
 }
